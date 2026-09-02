4fd9e6997b48f71b55a65d6f7dcdae0d0bd192bb
mohammedaldelemi78-byte <mohammedaldelemi78@gmail.com>
2026-09-02 09:46:35 +0200

    Add project files.

---
+++ Upgift 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System; // vi använder systemet
+namespace Upgift1
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("du är nästan klar med kap 1."); // coden säger vad som kommer när vi trycker på start pogram
+            Console.WriteLine("du har lärt dig skriva ut tecken som \" och \/" ); 
+
+
+
+
+        }
+        
+           
+
+   
+           
+
+    }
+
+
+
+
+
+
+
+}
